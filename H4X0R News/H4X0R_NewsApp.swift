5df56af02e6e0f34e5b5113c5c559671927adeb8
Ilyas Kudaibergenov <ilyassskb@gmail.com>
2022-04-19 05:59:07 +0600

    Initial Commit

---
+++ H4X0R News/H4X0R_NewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  H4X0R_NewsApp.swift
+//  H4X0R News
+//
+//  Created by Ильяс Кудайбергенов on 18.04.2022.
+//
+
+import SwiftUI
+
+@main
+struct H4X0R_NewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ H4X0R News/Views/ContentView.swift
@@ -0,0 +1,37 @@
+//
+//  ContentView.swift
+//  H4X0R News
+//
+//  Created by Ильяс Кудайбергенов on 18.04.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    @ObservedObject var networkManager = NetworkManager()
+    
+    var body: some View {
+        NavigationView {
+            List(networkManager.posts) { post in
+                NavigationLink(destination: DetailView(url: post.url)) {
+                    HStack {
+                        Text(String(post.points))
+                        Text(post.title)
+                    }
+                }
+            }
+            .navigationBarTitle("H4X0R NEWS")
+        }.onAppear {
+            self.networkManager.fetchData()
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView().previewDevice(PreviewDevice(rawValue: "iPhone 12"))
+    }
+}
+
+
